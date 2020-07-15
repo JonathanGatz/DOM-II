fcc4e03024a2adf7597f2b15ce670f588f9c24d4
Jonathan Gatz <jonathangatz@Jonathans-MBP.attlocal.net>
2020-07-14 20:58:43 -0400

    test

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@
 
 // ****************************************************************************************************************************
 
+// First, i had to create the specific event listener I wanted
+
+
 const test1 = document.createElement('content-destination')
 
 test1.addEventListener('click', () => {
@@ -25,23 +28,27 @@ const test3 = document.createElement('intro')
 
 test3.addEventListener(`focus`, () => {
     event.target.style.color = "red"
-    console.log('Testing the Click')
+    console.log('Testing the Click Into')
 })
+
 
 
 const test4 = document.createElement('div')
 
 test4.addEventListener(`scroll`, () => {
     test4.style.backgroundColor = "pink"
-    console.log('Testing the Click')
+    console.log('Testing the Click Div')
 })
+
+
+// The, I used what I created to specifically target the elements i wanted to change. I used a counsol.log as referense. 
 
 
 const test5 = document.createElement('content-destination')
 
 test5.addEventListener(`resize`, () => {
     test5.style.borderRadius = "scale(50)"
-    console.log('Testing the Click')
+    console.log('Testing the Click Content Destination')
 })
 
 
@@ -49,7 +56,7 @@ const test6 = document.createElement('btn')
 
 test6.addEventListener(`dblclick`, () => {
     test6.style.opacity = "0.1"
-    console.log('Testing the Click')
+    console.log('Testing the Click btn')
 })
 
 
@@ -57,7 +64,7 @@ const test7 = document.createElement('p')
 
 test7.addEventListener(`select`, () => {
     test7.style.opacity = "0.1"
-    console.log('Testing the Click')
+    console.log('Testing the Click p')
 })
 
 
@@ -65,7 +72,7 @@ const test8 = document.createElement('footer')
 
 test8.addEventListener(`mouseover`, () => {
     test8.style.scale = "300"
-    console.log('Testing the Click')
+    console.log('Testing the Click footer')
 })
 
 
@@ -73,7 +80,7 @@ const test9 = document.createElement('h4')
 
 test9.addEventListener(`drag / drop`, () => {
     test9.style.backgroundColor = "blue"
-    console.log('Testing the Click')
+    console.log('Testing the Click h4')
 })
 
 
@@ -81,10 +88,14 @@ const test10 = document.createElement('h2')
 
 test10.addEventListener(`scroll`, () => {
     test10.style.borderColor = "red"
-    console.log('Testing the Click')
+    console.log('Testing the Click h2')
 })
 
+// I feel it's easier to group everything together like this. It's easier for me to locate throught the file 
+
 // ****************************************************************************************************************************
+
+// I decided to switch it up a bit with actual functions... I know it's not recomended but I wanted to try it out
 
 
 function logo1 (logo) {
@@ -102,3 +113,7 @@ const stop = document.querySelector('a').addEventListener("click", function(even
     event.preventDefault();
 })
 
+
+// I used the "a" to put a stop to all of the links on the page 
+
+// ****************************************************************************************************************************
